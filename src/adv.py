9b1be83cfed99d0d5a779782b1479e13ff8e239a
Pherpher089 <Christutor089@gmail.com>
2020-01-23 19:52:23 -0800

    Set up REPL Loop

--- src/adv.py
+++ src/adv.py
@@ -1,7 +1,8 @@
+import os
 from room import Room
+from player import Player
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
@@ -13,7 +14,8 @@ passages run north and east."""),
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from \
+        west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
@@ -23,7 +25,6 @@ earlier adventurers. The only exit is to the south."""),
 
 
 # Link rooms together
-
 room['outside'].n_to = room['foyer']
 room['foyer'].s_to = room['outside']
 room['foyer'].n_to = room['overlook']
@@ -32,6 +33,9 @@ room['overlook'].s_to = room['foyer']
 room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
+
+possible_inputs = "\nMovements:\n\n [n] = Northy\n [e] = East \n [s] = South \
+    \n [w] = West\n\n\nGame Commands:\n\n [q] = quit"
 
 #
 # Main
@@ -49,3 +53,56 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+player = Player(room['outside'])
+user_input = None
+
+# Clear the terminal
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# Print the title
+print("\n\n  Welcome to \n    THE GAME")
+
+# Begin REPL loop
+while user_input != "q":
+
+    # if this is not the first loop, determined by the user_input clear the console
+    if(user_input != None):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Begin with printing the room
+    print(player.room)
+
+    # Prompt the player for user for input
+    user_input = input(f"\nWhat direction would you like to go? \n ->")
+
+    if(user_input.lower() == 'n'):
+        if(player.room.n_to != None):
+            player.room = player.room.n_to
+        else:
+            print(f'{player.room.get_name()} does not have a pasage to  the \
+                North')
+    elif(user_input.lower() == 'e'):
+        if(player.room.e_to != None):
+            player.room = player.room.e_to
+        else:
+            print(f'{player.room.get_name()} does not have a pasage to the \
+                East')
+    elif(user_input.lower() == 's'):
+        if(player.room.s_to != None):
+            player.room = player.room.s_to
+        else:
+            print(f'{player.room.get_name()} does not have a pasage to the \
+                South')
+    elif(user_input.lower() == 'w'):
+        if(player.room.w_to != None):
+            player.room = player.room.w_to
+        else:
+            print(f'{player.room.get_name()} does not have a pasage to the \
+                West')
+    elif(user_input.lower() == 'help'):
+        print(possible_inputs)
+    elif(user_input.lower() != 'q'):
+        print('\nPlease enter a comand\nor type help for more information')
+
+    print("\n")  # Alwase end the loop with a new line
--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, room):
+        self.room = room
+
+    def __repr__(self):
+        return f'current room: {self.room}'
